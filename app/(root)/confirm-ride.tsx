3c140a03c2fc71631775914ea7ed4de72ae93784
Nguyen Van Tinh <nguyenvantinh06@gmail.com>
2024-12-20 15:50:35 +0700

    Add Confirm Ride Screen

--- app/(root)/confirm-ride.tsx
+++ app/(root)/confirm-ride.tsx
@@ -1,10 +1,37 @@
-import { Text, View } from "react-native";
+import { router } from "expo-router";
+import { FlatList, View } from "react-native";
+
+import CustomButton from "@/components/CustomButton";
+import DriverCard from "@/components/DriverCard";
+import RideLayout from "@/components/RideLayout";
+import { useDriverStore } from "@/store";
+import { driversData } from "@/mockData/driver";
 
 const ConfirmRide = () => {
+  const { drivers, selectedDriver, setSelectedDriver } = useDriverStore();
+  const driversTest = driversData;
   return (
-    <View>
-      <Text>Confirm ride</Text>
-    </View>
+    <RideLayout title={"Choose a Rider"} snapPoints={["65%", "85%"]}>
+      <FlatList
+        data={driversTest}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item, index }) => (
+          <DriverCard
+            item={item}
+            selected={selectedDriver!}
+            setSelected={() => setSelectedDriver(item.id!)}
+          />
+        )}
+        ListFooterComponent={() => (
+          <View className="mx-5 mt-10">
+            <CustomButton
+              title="Select Ride"
+              onPress={() => router.push("/(root)/book-ride")}
+            />
+          </View>
+        )}
+      />
+    </RideLayout>
   );
 };
 
